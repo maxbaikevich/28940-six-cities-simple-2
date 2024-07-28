a8f545f1c9959fc0fdc1a78c86a69eda1cffa1e0
max.b <max.b@dev.izibook.ru>
2024-07-28 19:27:00 +0300

    cli start

---
+++ src/cli.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+import VersionCommand from './cli-command/version-command.js';
+import HelpCommand from './cli-command/help-command.js';
+// import ImportCommand from './cli-command/import-command.js';
+// import GenerateCommand from './cli-command/generate-command.js';
+import CLIApplication from './app/cli-application.js';
+
+const myManager = new CLIApplication();
+myManager.registerCommands([
+  new HelpCommand, new VersionCommand,
+
+]);
+myManager.processCommand(process.argv);
