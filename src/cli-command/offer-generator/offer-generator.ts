ec193a510f2fdd1b1a67d97de484eca3a7fa4cd8
max.b <max.b@dev.izibook.ru>
2024-08-12 23:53:39 +0300

    Генератор объявлений

--- src/cli-command/offer-generator/offer-generator.ts
+++ src/cli-command/offer-generator/offer-generator.ts
@@ -1,15 +1,21 @@
 import {generateRandomValue, getRandomItems, getRandomItem} from '../../utils/random.js';
 import {OfferGeneratorInterface} from '../offer-generator/offer-generatoe.interface.js';
-import {MockData} from '../../types/mock-data.type.js'
+import {MockData} from '../../types/mock-data.type.js';
+import dayjs from 'dayjs';
 
-const MIN_PRICE = 500;
-const MAXX_PRICE = 2000;
+
+const FIRST_WEEK_DAY = 1;
+const LAST_WEEK_DAY = 7;
 
 export default class OfferGenerate implements OfferGeneratorInterface {
   constructor(private readonly mockData: MockData) {}
 
   public generate(): string {
-    const titles =  getRandomItem<string>(this.mockData.titles).join(';');
-    return [titles].join('/t');
+    const title =  getRandomItem<string>(this.mockData.titles);
+    const description =  getRandomItem<string>(this.mockData.descriptions);
+    // const date = generateRandomValue<string>(this.mockData.cities);
+    const createDate = dayjs().subtract(generateRandomValue(FIRST_WEEK_DAY, LAST_WEEK_DAY), 'day').toISOString();
+    const city = getRandomItem<string>(this.mockData.cities);
+    return [title, description, createDate,city,].join('/t');
   }
 }
